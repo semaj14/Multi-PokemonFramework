a50f9872f0ea3de825e976cecf2999acf96f8326
Jared0714 <81500902+semaj14@users.noreply.github.com>
2023-03-19 23:33:06 -0700

    Foundations for multi-language support

--- Library/Sources/Framework/Language.cpp
+++ Library/Sources/Framework/Language.cpp
@@ -3,15 +3,34 @@
 #include "../../../Includes/stdafx.hpp"
 
 namespace CTRPluginFramework {
+    // Define an enumeration for supported languages
     enum Lang {
-        ENG,
-        FRE
+        ENG, // English
+        FRE  // French
     };
 
-    // Global to keep the current language
-    Lang g_language = ENG;
+    // Language class to manage the current language setting and provide translation functionality
+    class Language {
+        public:
+            // Constructor to initialize the current language to English by default
+            Language() : m_currentLanguage(ENG) {}
 
-    string AutoLanguage(string english, string french) {
-        return (g_language == ENG ? english : french);
-    }
+            // Function to set the current language
+            void setLanguage(Lang lang) {
+                m_currentLanguage = lang;
+            }
+
+            // Function to get the current language
+            Lang getLanguage() const {
+                return m_currentLanguage;
+            }
+
+            // Function to automatically choose the correct translation based on the current language setting
+            string autoLanguage(string english, string french) const {
+                return (m_currentLanguage == ENG ? english : french);
+            }
+
+        private:
+            Lang m_currentLanguage; // Member variable to store the current language setting
+    };
 }
